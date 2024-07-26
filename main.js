f0985438b800a1724f8f36fd687aa7727994fd42
Padmanaban.M <pappu20012908@gmail.com>
2024-07-26 10:24:42 +0530

    js array sorting

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// let person =["Ranjith","akhila","pappu"];
+
+// console.log(person[1]);
+// console.log(person[2]);
+
+// person[0]="arun";
+
+// console.log(person);
+  
+let team=["Rcb","Gt","Csk"];
+
+console.log(team[0]);
+console.log(team[2]);
+
+team[1]="MI";
+console.log(team);
